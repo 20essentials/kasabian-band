eb3454c45fb6c3644167cddaddfd37fbeb3f0ac9
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:05:38 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KASABIAN';
+export let nameSongs = [
+	"CLUB FOOT", /*1*/
+	"PROCESSED BEATS", /*2*/
+	"FIRE", /*3*/
+	"UNDERDOG", /*4*/
+	"L.S.F. (LOST SOULS FOREVER)", /*5*/
+	"STEVIE", /*6*/
+	"RE-WIRED", /*7*/
+	"DAYS ARE FORGOTTEN", /*8*/
+	"WHERE DID ALL THE LOVE GO?", /*9*/
+	"FAST FUSE", /*10*/
+	"LET'S ROLL JUST LIKE WE USED TO", /*11*/
+	"WASTED", /*12*/
+	"LA FEE VERTE", /*13*/
+	"ARE YOU LOOKING FOR ACTION?", /*14*/
+	"YOU'RE IN LOVE WITH A PSYCHO", /*15*/
+	"BLESS THIS ACID HOUSE", /*16*/
+	"STUNTMAN", /*17*/
+	"GOOD FIGHT", /*18*/
+	"THICK AS THIEVES", /*19*/
+	"COMING BACK TO ME GOOD", /*20*/
+]
